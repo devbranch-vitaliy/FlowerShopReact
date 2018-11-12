c0d3c57a11499dabc5c3de2545dd32ba496e3b5e
HitchShock <RenegatDeath@gmail.com>
2018-11-13 00:59:00 +0200

    PDF-16. Changed JS Code for several situation: profile payment, order payment

--- drupal/web/modules/custom/fleur_address/js/address_type.js
+++ drupal/web/modules/custom/fleur_address/js/address_type.js
@@ -4,57 +4,65 @@
  */
 
 (function ($, Drupal, debounce) {
-  Drupal.behaviors.address_type = {
-    attach: function attach(context, settings) {
+    Drupal.behaviors.address_type = {
+        attach: function attach(context, settings) {
 
-        var idList = {
-            'profile': {
-                wrapper         : '#edit-field-address-type',
-                company_field   : '.form-item-address-0-address-organization',
-                company_value   : '#edit-address-0-address-organization',
-            },
-            'shipping': {
-                wrapper         : '#edit-shipping-information-shipping-profile-field-address-type',
-                company_field   : '.form-item-shipping-information-shipping-profile-address-0-address-organization',
-                company_value   : '#edit-shipping-information-shipping-profile-address-0-address-organization',
-            },
-            'payment': {
-                wrapper         : '#edit-payment-information-add-payment-method-billing-information-field-address-type',
-                company_field   : '.form-item-payment-information-add-payment-method-billing-information-address-0-address-organization',
-                company_value   : '#edit-payment-information-add-payment-method-billing-information-address-0-address-organization',
-            },
-        };
+            var idList = {
+                'profile': {
+                    wrapper         : '#edit-field-address-type',
+                    company_field   : '.form-item-address-0-address-organization',
+                    company_value   : '#edit-address-0-address-organization',
+                },
+                'profile_payment': {
+                    wrapper         : '#edit-payment-method-billing-information-field-address-type',
+                    company_field   : '.form-item-payment-method-billing-information-address-0-address-organization',
+                    company_value   : '#edit-payment-method-billing-information-address-0-address-organization',
+                },
+                'order_payment': {
+                    wrapper         : '#edit-billing-profile-0-profile-field-address-type',
+                    company_field   : '.form-item-billing-profile-0-profile-address-0-address-organization',
+                    company_value   : '#edit-billing-profile-0-profile-address-0-address-organization',
+                },
+                'shipping': {
+                    wrapper         : '#edit-shipping-information-shipping-profile-field-address-type',
+                    company_field   : '.form-item-shipping-information-shipping-profile-address-0-address-organization',
+                    company_value   : '#edit-shipping-information-shipping-profile-address-0-address-organization',
+                },
+                'payment': {
+                    wrapper         : '#edit-payment-information-add-payment-method-billing-information-field-address-type',
+                    company_field   : '.form-item-payment-information-add-payment-method-billing-information-address-0-address-organization',
+                    company_value   : '#edit-payment-information-add-payment-method-billing-information-address-0-address-organization',
+                },
+            };
 
-        function companyVisible(obj){
-            let compoane_field = $(obj.company_field);
+            function companyVisible(obj){
+                let compoane_field = $(obj.company_field);
 
-            // Home option
-            if ($(obj.wrapper+'-0[checked="checked"]').length && !compoane_field.hasClass('hide')) compoane_field.addClass('hide');
+                // Home option
+                if ($(obj.wrapper+'-0[checked="checked"]').length && !compoane_field.hasClass('hide')) compoane_field.addClass('hide');
 
-            // Company option
-            if ($(obj.wrapper+'-1[checked="checked"]').length && compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
+                // Company option
+                if ($(obj.wrapper+'-1[checked="checked"]').length && compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
+            }
+
+            Object.values(idList).forEach(function (value) {
+                let compoane_field = $(value.company_field);
+
+                $(value.wrapper).once().each(function() {
+                    companyVisible(value);
+                });
+
+                $(value.wrapper+'-0').once().on('click', function () {
+                    if (!compoane_field.hasClass('hide')) compoane_field.addClass('hide');
+                    let company_value = $(value.company_value);
+                    if (company_value.length) company_value[0].value = "";
+                });
+
+                $(value.wrapper+'-1').once().on('click', function () {
+                    if (compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
+                });
+            })
+
         }
-
-        Object.values(idList).forEach(function (value) {
-            let compoane_field = $(value.company_field);
-
-            $(value.wrapper).once().each(function() {
-                companyVisible(value);
-            });
-
-            // Home click
-            $(value.wrapper+'-0').once().on('click', function () {
-                if (!compoane_field.hasClass('hide')) compoane_field.addClass('hide');
-                let company_value = $(value.company_value);
-                if (company_value.length) company_value[0].value = "";
-            });
-
-            // Company click
-            $(value.wrapper+'-1').once().on('click', function () {
-                if (compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
-            });
-        })
-
-    }
-  };
+    };
 })(jQuery, Drupal, Drupal.debounce);
