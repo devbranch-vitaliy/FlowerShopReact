b584311b25f9d1c841751b9571adecc15a49018f
HitchShock <RenegatDeath@gmail.com>
2018-11-13 00:17:59 +0200

    PDF-16. Changed JS Code for several situation: customer profile, shipping, payment

--- drupal/web/modules/custom/fleur_address/js/address_type.js
+++ drupal/web/modules/custom/fleur_address/js/address_type.js
@@ -7,26 +7,53 @@
   Drupal.behaviors.address_type = {
     attach: function attach(context, settings) {
 
-        var compoane_field = $('.form-item-address-0-address-organization');
+        var idList = {
+            'profile': {
+                wrapper         : '#edit-field-address-type',
+                company_field   : '.form-item-address-0-address-organization',
+                company_value   : '#edit-address-0-address-organization',
+            },
+            'shipping': {
+                wrapper         : '#edit-shipping-information-shipping-profile-field-address-type',
+                company_field   : '.form-item-shipping-information-shipping-profile-address-0-address-organization',
+                company_value   : '#edit-shipping-information-shipping-profile-address-0-address-organization',
+            },
+            'payment': {
+                wrapper         : '#edit-payment-information-add-payment-method-billing-information-field-address-type',
+                company_field   : '.form-item-payment-information-add-payment-method-billing-information-address-0-address-organization',
+                company_value   : '#edit-payment-information-add-payment-method-billing-information-address-0-address-organization',
+            },
+        };
 
-        function companyVisible(){
-            if ($('#edit-field-address-type-0[checked="checked"]').length && !compoane_field.hasClass('hide')) compoane_field.addClass('hide');
-            if ($('#edit-field-address-type-1[checked="checked"]').length && compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
+        function companyVisible(obj){
+            let compoane_field = $(obj.company_field);
+
+            // Home option
+            if ($(obj.wrapper+'-0[checked="checked"]').length && !compoane_field.hasClass('hide')) compoane_field.addClass('hide');
+
+            // Company option
+            if ($(obj.wrapper+'-1[checked="checked"]').length && compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
         }
 
-        $('#edit-field-address-type').once().each(function() {
-            companyVisible();
-        });
+        Object.values(idList).forEach(function (value) {
+            let compoane_field = $(value.company_field);
 
-        $('#edit-field-address-type-0').once().on('click', function () {
-            if (!compoane_field.hasClass('hide')) compoane_field.addClass('hide');
-            let company_value = $('#edit-address-0-address-organization');
-            if (company_value.length) company_value[0].value = "";
-        });
+            $(value.wrapper).once().each(function() {
+                companyVisible(value);
+            });
 
-        $('#edit-field-address-type-1').once().on('click', function () {
-            if (compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
-        });
+            // Home click
+            $(value.wrapper+'-0').once().on('click', function () {
+                if (!compoane_field.hasClass('hide')) compoane_field.addClass('hide');
+                let company_value = $(value.company_value);
+                if (company_value.length) company_value[0].value = "";
+            });
+
+            // Company click
+            $(value.wrapper+'-1').once().on('click', function () {
+                if (compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
+            });
+        })
 
     }
   };
