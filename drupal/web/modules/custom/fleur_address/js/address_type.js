5cd41618e6ace8a2573a3f7aaea60fc6728abc88
Vadym Abramchuk <vadym@dev-branch.com>
2018-11-13 08:59:37 +0200

    PDF-16 Simplify JS code

--- drupal/web/modules/custom/fleur_address/js/address_type.js
+++ drupal/web/modules/custom/fleur_address/js/address_type.js
@@ -1,68 +1,29 @@
 /**
- * Change cart flyout function
- * @preserve
+ * Show/hide Company field based on the Address Type field value.
  */
 
-(function ($, Drupal, debounce) {
-    Drupal.behaviors.address_type = {
-        attach: function attach(context, settings) {
-
-            var idList = {
-                'profile': {
-                    wrapper         : '#edit-field-address-type',
-                    company_field   : '.form-item-address-0-address-organization',
-                    company_value   : '#edit-address-0-address-organization',
-                },
-                'profile_payment': {
-                    wrapper         : '#edit-payment-method-billing-information-field-address-type',
-                    company_field   : '.form-item-payment-method-billing-information-address-0-address-organization',
-                    company_value   : '#edit-payment-method-billing-information-address-0-address-organization',
-                },
-                'order_payment': {
-                    wrapper         : '#edit-billing-profile-0-profile-field-address-type',
-                    company_field   : '.form-item-billing-profile-0-profile-address-0-address-organization',
-                    company_value   : '#edit-billing-profile-0-profile-address-0-address-organization',
-                },
-                'shipping': {
-                    wrapper         : '#edit-shipping-information-shipping-profile-field-address-type',
-                    company_field   : '.form-item-shipping-information-shipping-profile-address-0-address-organization',
-                    company_value   : '#edit-shipping-information-shipping-profile-address-0-address-organization',
-                },
-                'payment': {
-                    wrapper         : '#edit-payment-information-add-payment-method-billing-information-field-address-type',
-                    company_field   : '.form-item-payment-information-add-payment-method-billing-information-address-0-address-organization',
-                    company_value   : '#edit-payment-information-add-payment-method-billing-information-address-0-address-organization',
-                },
-            };
-
-            function companyVisible(obj){
-                let compoane_field = $(obj.company_field);
-
-                // Home option
-                if ($(obj.wrapper+'-0[checked="checked"]').length && !compoane_field.hasClass('hide')) compoane_field.addClass('hide');
-
-                // Company option
-                if ($(obj.wrapper+'-1[checked="checked"]').length && compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
-            }
-
-            Object.values(idList).forEach(function (value) {
-                let compoane_field = $(value.company_field);
-
-                $(value.wrapper).once().each(function() {
-                    companyVisible(value);
-                });
-
-                $(value.wrapper+'-0').once().on('click', function () {
-                    if (!compoane_field.hasClass('hide')) compoane_field.addClass('hide');
-                    let company_value = $(value.company_value);
-                    if (company_value.length) company_value[0].value = "";
-                });
-
-                $(value.wrapper+'-1').once().on('click', function () {
-                    if (compoane_field.hasClass('hide')) compoane_field.removeClass('hide');
-                });
-            })
-
-        }
-    };
-})(jQuery, Drupal, Drupal.debounce);
+(function ($, Drupal) {
+  Drupal.behaviors.fleur_address_type = {
+    attach: function attach(context, settings) {
+      $('.field--name-field-address-type input[type=radio]', context).each(function (e) {
+        var $this = $(this);
+        var showHideCompanyField = function ($input) {
+          if (!$input.is(':checked')) {
+            return;
+          }
+          var $field_wrapper = $this.closest('.field--name-field-address-type');
+          var $address_wrapper = $field_wrapper.parent().find('.field--type-address');
+          var $company_input = $address_wrapper.find('input.organization').closest('.form-item');
+          if ($this.val() == 1) {
+            $company_input.show();
+          }
+          else {
+            $company_input.hide();
+          }
+        };
+        showHideCompanyField($this);
+        $this.once('address-type').change(function() {showHideCompanyField($this)});
+      });
+    }
+  };
+})(jQuery, Drupal);
