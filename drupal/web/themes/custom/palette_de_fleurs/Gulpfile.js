578b13c2c7924304a729a38dad89a67331340962
VitaliiPodoba <v.podoba@dev-branch.com>
2018-12-27 20:53:24 +0200

    PDF-89 Configured docker-compose.yml and Gulpfile.js

--- drupal/web/themes/custom/palette_de_fleurs/Gulpfile.js
+++ drupal/web/themes/custom/palette_de_fleurs/Gulpfile.js
@@ -1,13 +1,40 @@
+/**
+ * @file
+ *
+ * @type {Gulp}
+ */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var svgSprite = require('gulp-svg-sprite');
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   gulp.src('scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css/'))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css/'))
 });
 
-//Watch task
-gulp.task('default',function() {
+// Configuration for sprite.
+var config = {
+  mode: {
+    css: {
+      render: {
+        scss: {
+          dest:'icons/_fleur_icons.scss',
+        },
+      },
+    },
+  }
+};
+
+gulp.task('sprite', function () {
+  gulp.src('assets/icons/*.svg')
+      .pipe(svgSprite(config))
+      .pipe(gulp.dest('scss'));
+});
+
+// Watch task.
+gulp.task('default',function () {
   gulp.watch('scss/**/*.scss',['styles']);
+  gulp.watch('assets/icons/*.svg',['sprite']);
 });
