366b37ed481b4a94ecb57473833d2bc36b61729b
VitaliiPodoba <v.podoba@dev-branch.com>
2018-12-28 16:24:24 +0200

    PDF-89 Added gulp-iconfont and gulp-iconfont-css modules, configured new task for fonts creation from converted svg icons, added template for creation scss file, rebuilded styles

--- drupal/web/themes/custom/palette_de_fleurs/Gulpfile.js
+++ drupal/web/themes/custom/palette_de_fleurs/Gulpfile.js
@@ -7,11 +7,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var svgSprite = require('gulp-svg-sprite');
+var iconfont = require('gulp-iconfont');
+var iconfontCss = require('gulp-iconfont-css');
 
 gulp.task('styles', function () {
   gulp.src('scss/**/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./css/'))
+});
+
+var fontName = 'FleurIcons';
+
+gulp.task('iconfont', function () {
+  gulp.src(['assets/icons-convert/*.svg'])
+      .pipe(iconfontCss({
+        fontName: fontName,
+        path: 'assets/sass/templates/_fleur_icon_style.scss',
+        targetPath: '../../scss/icons/_fleur_icons_font.scss',
+        fontPath: '#{$theme-root}/fonts/FleurIcons/',
+        cssClass: 'fleur-font'
+      }))
+      .pipe(iconfont({
+        fontName: fontName,
+        formats: ['svg', 'ttf', 'eot', 'woff'],
+        normalize: true,
+        fontHeight: 1001
+      }))
+      .pipe(gulp.dest('fonts/FleurIcons'))
 });
 
 // Configuration for sprite.
@@ -22,6 +44,7 @@ var config = {
       sprite: '../images/sprite.svg',
       bust: false,
       prefix: ".fleur-%s",
+      mixin: 'sprite-content',
       render: {
         scss: {
           dest:'icons/_fleur_icons.scss',
