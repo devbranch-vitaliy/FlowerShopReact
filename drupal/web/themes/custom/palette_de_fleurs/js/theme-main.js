2de7c34f15daf6c4e8be29cfd350ae45cac13e89
VitaliiPodoba <v.podoba@dev-branch.com>
2019-01-04 20:12:24 +0200

    PDF-74 Wrote JS code for background color and popup

---
+++ drupal/web/themes/custom/palette_de_fleurs/js/theme-main.js
@@ -0,0 +1,37 @@
+/**
+ * @file
+ * Main js code for our theme.
+ */
+
+(function ($, Drupal, window) {
+
+  Drupal.behaviors.fleur_theme = {
+    attach: function (context, settings) {
+      // Header background color.
+      var set_background = function () {
+        var $header = $('#header-background');
+        if ($(window).scrollTop() > $header.height()) {
+          $header.addClass('scrolling');
+        }
+        else {
+          $header.removeClass('scrolling');
+        }
+      };
+
+      $(document).once().ready(function () {
+        set_background();
+      });
+
+      $(window).once().scroll(function () {
+        set_background();
+      });
+
+      // Show popup.
+      $('.call-block').once().click(function () {
+        // $('#call-block-popup').toggle(1000);
+        document.getElementById('call-block-popup').classList.toggle('show');
+      })
+    }
+  };
+
+})(jQuery, Drupal, window);
