cb9ed567f71587543813cf5fad37d6308e891629
VitaliiPodoba <v.podoba@dev-branch.com>
2019-01-18 15:49:51 +0200

    PDF-90 Created style for FAQ page

--- drupal/web/themes/custom/palette_de_fleurs/js/faq-questions.js
+++ drupal/web/themes/custom/palette_de_fleurs/js/faq-questions.js
@@ -6,7 +6,18 @@
 (function ($, Drupal) {
     Drupal.behaviors.fleurMobileFilter = {
         attach: function (context, settings) {
+            $('.question', context).once().each(function () {
+                var $container = $(this);
 
+                $('.views-field-field-question', $container).click(function (e) {
+                    var $this = $(this);
+
+                    $this.next().slideToggle(350);
+                    $this.next().toggleClass('show');
+                    $this.toggleClass('icon-up');
+
+                });
+            })
         }
     }
 })(jQuery, Drupal);
