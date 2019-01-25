fa8eecaa4bc3ff6462496eae255ec291c0a20333
VitaliiPodoba <v.podoba@dev-branch.com>
2019-01-25 12:38:55 +0200

    PDF-96 Created links in new block

--- drupal/web/modules/custom/fleur_node_pager/src/Plugin/Block/NodePager.php
+++ drupal/web/modules/custom/fleur_node_pager/src/Plugin/Block/NodePager.php
@@ -3,6 +3,9 @@
 namespace Drupal\fleur_node_pager\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
 
 /**
  * Created block with pager for node page.
@@ -22,12 +25,72 @@ class NodePager extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    /** @var \Drupal\node\Entity\Node $node */
     $node = $this->getContextValue('node');
 
-    $block = [
-      '#type' => 'markup',
-      '#markup' => $this->t('test text'),
+    // Get previous nodes.
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', $node->getType())
+      ->condition('changed', $node->get('changed')->value, '<')
+      ->sort('changed', 'DESC')
+      ->sort('nid', 'DESC')
+      ->pager(1);
+
+    $previous = $query->execute();
+
+    // Get next nodes.
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', $node->getType())
+      ->condition('changed', $node->get('changed')->value, '>')
+      ->sort('changed', 'ASC')
+      ->sort('nid', 'ASC')
+      ->pager(1);
+
+    $next = $query->execute();
+
+    // Create cacheable metadata.
+    $cache = new CacheableMetadata();
+
+    // Create elements.
+    $block['pager_links'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => ['pager-links'],
+      ],
     ];
+
+    if ($previous) {
+      $id = reset($previous);
+      $link_node = Node::load($id);
+      $cache->addCacheableDependency($link_node);
+
+      $block['pager_links']['previous_link'] = [
+        '#title' => $link_node->getTitle(),
+        '#type' => 'link',
+        '#url' => Url::fromRoute('entity.node.canonical', ['node' => $id], ['absolute' => TRUE]),
+        '#attributes' => [
+          'class' => ['previous-links'],
+        ],
+      ];
+    }
+
+    if ($next) {
+      $id = reset($next);
+      $link_node = Node::load($id);
+      $cache->addCacheableDependency($link_node);
+
+      $block['pager_links']['next_link'] = [
+        '#title' => Node::load($id)->getTitle(),
+        '#type' => 'link',
+        '#url' => Url::fromRoute('entity.node.canonical', ['node' => $id], ['absolute' => TRUE]),
+        '#attributes' => [
+          'class' => ['next-links'],
+        ],
+      ];
+    }
+
+    $cache->applyTo($block);
+
     return $block;
   }
 
