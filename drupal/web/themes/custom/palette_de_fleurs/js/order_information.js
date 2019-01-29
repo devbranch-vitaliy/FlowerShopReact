62f3b0a686b8c2d68451d7cde255fb024e652b28
VitaliiPodoba <v.podoba@dev-branch.com>
2019-01-29 18:18:31 +0200

    PDF-100 Created new library for Order information page

---
+++ drupal/web/themes/custom/palette_de_fleurs/js/order_information.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * JS code for Order information page.
+ */
+
+(function ($, Drupal) {
+    Drupal.behaviors.fleurOrderInformation = {
+        attach: function (context, settings) {
+
+        }
+    }
+})(jQuery, Drupal);
