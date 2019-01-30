9f7f2e4bf6b05438fdea4d4fabe22f8397333d5e
Vadym Abramchuk <vadym@dev-branch.com>
2019-01-30 09:47:28 +0200

    PDF-100 Checkout date/time picker

---
+++ drupal/web/themes/custom/palette_de_fleurs/js/fleur-datepicker.js
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * Datepicker styling JS.
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.fleurDatepicker = {
+    attach: function (context, settings) {
+      $('input[type=date]', context).once('fleur-datepicker').each(function () {
+        var $dateInput = $(this);
+        var $textInput = $('<input/>')
+          .attr('type', 'text')
+          .attr('placeholder', Drupal.t('Select date'))
+          .addClass('form-control');
+
+        var $inputWrapper = $('<div />')
+          .addClass('form-group has-feedback')
+          .append($textInput);
+
+        $inputWrapper.insertAfter($dateInput);
+        $textInput.val($dateInput.val());
+
+        var $icon = $('<i class="datepicker-icon glyphicon glyphicon-calendar form-control-feedback"></i>');
+        $icon.insertAfter($textInput);
+
+        var defaultDate = $dateInput.val() ? new Date($dateInput.val()) : undefined;
+        var $datepicker = $textInput.datetimepicker({
+          format: 'L',
+          // Current date.
+          minDate: new Date(),
+          showClose: true,
+          date: defaultDate,
+        });
+//        $textInput.change(function () {
+//          $dateInput.val($textInput.val());
+//        });
+        $textInput.on('dp.change', function () {
+          $dateInput.val($textInput.data("DateTimePicker").viewDate().format('YYYY-MM-DD'));
+//          console.log($datepicker.viewDate());
+        });
+      });
+
+    }
+  }
+})(jQuery, Drupal);
