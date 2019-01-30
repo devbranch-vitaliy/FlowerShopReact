10c47f2fa77a6dafc9ac151440a1ccd84d0ce79f
Vadym Abramchuk <vadym@dev-branch.com>
2019-01-30 09:48:11 +0200

    PDF-100 Cleanup

--- drupal/web/themes/custom/palette_de_fleurs/js/fleur-datepicker.js
+++ drupal/web/themes/custom/palette_de_fleurs/js/fleur-datepicker.js
@@ -24,19 +24,15 @@
         $icon.insertAfter($textInput);
 
         var defaultDate = $dateInput.val() ? new Date($dateInput.val()) : undefined;
-        var $datepicker = $textInput.datetimepicker({
+        $textInput.datetimepicker({
           format: 'L',
           // Current date.
           minDate: new Date(),
           showClose: true,
           date: defaultDate,
         });
-//        $textInput.change(function () {
-//          $dateInput.val($textInput.val());
-//        });
         $textInput.on('dp.change', function () {
           $dateInput.val($textInput.data("DateTimePicker").viewDate().format('YYYY-MM-DD'));
-//          console.log($datepicker.viewDate());
         });
       });
 
